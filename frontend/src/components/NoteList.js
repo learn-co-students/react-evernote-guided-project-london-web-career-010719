bd6208d774e8b6d71d86bbae48e059535568b2c3
YannickHoegerle <yannick.hoegerle@gmail.com>
2019-03-25 21:16:17 +0000

    Done.

--- frontend/src/components/NoteList.js
+++ frontend/src/components/NoteList.js
@@ -3,16 +3,15 @@ import NoteItem from './NoteItem';
 
 export default class NoteList extends React.Component {
 
-
   renderList () {
-    const matchSearch = this.props.allNotes.map(note => note.title.toLowerCase().includes(this.props.searchTerm.toLowerCase()))
+    const matchSearch = this.props.allNotes.filter(note => note.title.toLowerCase().includes(this.props.searchTerm.toLowerCase()))
     const userNotes = this.props.allNotes.filter(note => note.user_id === this.props.currentUser.id)
 
-    if (this.props.searchTerm === '') {
+    // if (this.props.searchTerm === '') {
       return userNotes.map(note => <NoteItem key={note.id} note={note} onClickViewNote={() => this.props.onClickViewNote(note)}/>)
-    } else {
-      return matchSearch.map(note => <NoteItem key={note.id} note={note} onClickViewNote={() => this.props.onClickViewNote(note)}/>)
-    }
+    // } else {
+    //   return matchSearch.map(note => <NoteItem key={note.id} note={note} onClickViewNote={() => this.props.onClickViewNote(note)}/>)
+    // }
   }
 
   render() {
