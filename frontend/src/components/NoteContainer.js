1511816459431b293e2bfb58026435b4219b2a8a
YannickHoegerle <yannick.hoegerle@gmail.com>
2019-03-25 15:32:20 +0000

    Done.

--- frontend/src/components/NoteContainer.js
+++ frontend/src/components/NoteContainer.js
@@ -2,14 +2,30 @@ import React, { Component, Fragment } from 'react';
 import Search from './Search';
 import Sidebar from './Sidebar';
 import Content from './Content';
+import API from '../adapters/API';
 
 class NoteContainer extends Component {
+
+  state = {
+    allUsers:[],
+    allNotes:[],
+    currentUser: '',
+  }
+
+  componentDidMount(){
+    API.getAllUsers()
+      .then(users => this.setState({allUsers: users}))
+      .then(() => this.setState({currentUser: this.state.allUsers[0]}))
+    API.getAllNotes()
+      .then(notes => this.setState({allNotes: notes}))
+  }
+
   render() {
     return (
       <Fragment>
         <Search />
         <div className='container'>
-          <Sidebar />
+          <Sidebar currentUser={this.state.currentUser} allNotes={this.state.allNotes}/>
           <Content />
         </div>
       </Fragment>
--- frontend/src/components/NoteList.js
+++ frontend/src/components/NoteList.js
@@ -4,8 +4,9 @@ import NoteItem from './NoteItem';
 const NoteList = (props) => {
   return (
     <ul>
-      {/* Render list of notes here... */}
-      <NoteItem />
+      {
+       props.allNotes.map(note => <NoteItem allNotes={props.allNotes}/>)
+      }
     </ul>
   );
 }
